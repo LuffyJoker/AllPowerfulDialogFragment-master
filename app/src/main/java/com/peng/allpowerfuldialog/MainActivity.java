52a2b5d08ef1da011e5a9ff56b4a2dd564fb2f2d
pengqi <m18380449235_1@163.com>
2019-03-15 11:50:17 +0800

    1、删除app中MainActivity示例代码

--- app/src/main/java/com/peng/allpowerfuldialog/MainActivity.java
+++ app/src/main/java/com/peng/allpowerfuldialog/MainActivity.java
@@ -31,37 +31,5 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
 
         setContentView(R.layout.activity_main);
-
-        textView = findViewById(R.id.tv_msg);
-        Button btn = findViewById(R.id.btn);
-        baseDialogFragment = new BaseDialogFragment();
-        DialogFragmentOptions options = new DialogFragmentOptions();
-        options.layoutId = R.layout.dialog_two_btn;
-        options.animStyle = R.style.ScaleOverShootEnterExitAnimationX100Y50;
-        options.gravityAsView = DialogGravity.LEFT_TOP;
-        options.asView = true;
-        options.touchCancel = true;
-        options.backCancel = false;
-        options.offsetX = 0;
-        options.offsetY = 0;
-        options.width = ConvertUtils.dp2px(100f);
-        options.height = ConvertUtils.dp2px(100f);
-        baseDialogFragment.setDialogFragmentOptions(options);
-
-    }
-
-
-    public void showDialogFragmentOnWindow(BaseDialogFragment baseDialogFragment, String tag) {
-        baseDialogFragment.showOnWindow(getSupportFragmentManager(), tag);
-    }
-
-    public void click(View view) {
-        showDialogFragmentOnWindow(baseDialogFragment, "123");
-    }
-
-    public void show(View view) {
-        Log.d(TAG, "show: "+textView.getX());
-        Log.d(TAG, "show: "+textView.getY());
-        baseDialogFragment.showOnView(getSupportFragmentManager(),textView,baseDialogFragment.getClass().getName(),true,true);
     }
 }
